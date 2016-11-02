6bc8f5aa46654e5c18ddd8bfa7790dd95a6fdf26
Rui Quelhas <rfpquelhas@gmail.com>
2016-11-02 20:57:05 +0000

    Update `multi-part` to the latest version

--- test/index.js
+++ test/index.js
@@ -53,7 +53,7 @@ lab.experiment('copperfield', () => {
         form.append('file3', Fs.createReadStream(gif));
         form.append('foo', 'bar');
 
-        server.inject({ headers: form.getHeaders(), method: 'POST', payload: form.get(), url: '/' }, (response) => {
+        server.inject({ headers: form.getHeaders(), method: 'POST', payload: form.stream(), url: '/' }, (response) => {
 
             Code.expect(response.statusCode).to.equal(400);
             Code.expect(response.headers['content-validation']).to.equal('failure');
@@ -76,7 +76,7 @@ lab.experiment('copperfield', () => {
         form.append('file2', Fs.createReadStream(png));
         form.append('foo', 'bar');
 
-        server.inject({ headers: form.getHeaders(), method: 'POST', payload: form.get(), url: '/' }, (response) => {
+        server.inject({ headers: form.getHeaders(), method: 'POST', payload: form.stream(), url: '/' }, (response) => {
 
             Code.expect(response.statusCode).to.equal(200);
             Code.expect(response.headers['content-validation']).to.equal('success');
